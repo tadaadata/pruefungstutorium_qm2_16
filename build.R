a5469cc3866fcd79281d57dfff5ada582957a97c
jemus42 <jemus42@quantenbrot.de>
2016-08-17 17:17:08 +0200

    The fanciest build script I ever buildscripted

--- build.R
+++ build.R
@@ -8,12 +8,23 @@ rmarkdown::render(input         = "presentation.Rmd",
                   output_format = "html_document",
                   output_file   = "Presentation_web.html")
 
-if (system(command = "whoami", intern = T) == "Lukas") {
-  file.copy("plots",                 to = "~/BitTorrent Sync/public.tadaa-data.de/QM/pruefungstutorien/qm2_sose_16/", overwrite = T, recursive = T)
-  file.copy("RStudio-Ball.png",      to = "~/BitTorrent Sync/public.tadaa-data.de/QM/pruefungstutorien/qm2_sose_16/", overwrite = F)
-  file.copy("styles.css",            to = "~/BitTorrent Sync/public.tadaa-data.de/QM/pruefungstutorien/qm2_sose_16/", overwrite = T)
-  file.copy("Presentation.html",     to = "~/BitTorrent Sync/public.tadaa-data.de/QM/pruefungstutorien/qm2_sose_16/", overwrite = T)
-  file.copy("Presentation_web.html", to = "~/BitTorrent Sync/public.tadaa-data.de/QM/pruefungstutorien/qm2_sose_16/", overwrite = T)
-  file.copy("presentation.Rmd",      to = "~/BitTorrent Sync/public.tadaa-data.de/QM/pruefungstutorien/qm2_sose_16/", overwrite = T)
+current_user <- system(command = "whoami", intern = T)
 
+if (current_user == "Lukas") {
+  out_dir <- "~/BitTorrent Sync/public.tadaa-data.de/QM/pruefungstutorien/qm2_sose_16/"
+} else if (current_user == "Tobi") {
+  out_dir <- ""
+} else {
+  out_dir <- ""
 }
+
+if (out_dir == "") {
+  stop("No output directory defined")
+}
+
+out_docs   <- c("Presentation.html", "Presentation_web.html", "presentation.Rmd")
+out_assets <- c("RStudio-Ball.png", "styles.css")
+
+sapply(out_docs,   file.copy, to = out_dir, overwrite = T, recursive = F)
+sapply(out_assets, file.copy, to = out_dir, overwrite = T, recursive = T)
+
