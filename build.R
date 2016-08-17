24b69c9392812931d3d47d604c197e0cbd4cf53a
jemus42 <jemus42@quantenbrot.de>
2016-08-17 16:21:04 +0200

    Copy to public.tadaa-data.de

--- build.R
+++ build.R
@@ -7,3 +7,11 @@ rmarkdown::render(input         = "presentation.Rmd",
 rmarkdown::render(input         = "presentation.Rmd",
                   output_format = "html_document",
                   output_file   = "Presentation_web.html")
+
+if (system(command = "whoami", intern = T) == "Lukas") {
+  file.copy("plots",                 to = "~/BitTorrent Sync/public.tadaa-data.de/QM/pruefungstutorien/qm2_sose_16/", overwrite = T, recursive = T)
+  file.copy("RStudio-Ball.png",      to = "~/BitTorrent Sync/public.tadaa-data.de/QM/pruefungstutorien/qm2_sose_16/", overwrite = F)
+  file.copy("styles.css",            to = "~/BitTorrent Sync/public.tadaa-data.de/QM/pruefungstutorien/qm2_sose_16/", overwrite = T)
+  file.copy("Presentation.html",     to = "~/BitTorrent Sync/public.tadaa-data.de/QM/pruefungstutorien/qm2_sose_16/", overwrite = T)
+  file.copy("Presentation_web.html", to = "~/BitTorrent Sync/public.tadaa-data.de/QM/pruefungstutorien/qm2_sose_16/", overwrite = T)
+}
